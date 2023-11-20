0744f1b7a6e70bdea483886b1dd3ab36c5ccd588
wawoo1234 <mogi1114@naver.com>
2023-11-20 10:49:21 +0900

    정수타입예제 추가

---
+++ src/typeTest01/Test01.java
@@ -0,0 +1,15 @@
+package typeTest01;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 3;
+		double result;
+		
+		result = (double) a/b;
+		System.out.println(result);
+	}
+
+}
---
+++ src/typeTest01/Test02.java
@@ -0,0 +1,19 @@
+package typeTest01;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "123";
+		int s1= Integer.parseInt(str);
+		double d1 = Double.parseDouble(str);
+		
+		System.out.println(s1);
+		System.out.println(d1);
+		
+		int i1 = 1234;
+		String str3 = String.valueOf(i1);
+		System.out.println(str3);
+	}
+
+}
